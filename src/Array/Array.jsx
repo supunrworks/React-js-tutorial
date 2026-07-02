6d0c048190e6261f6d64266ce1dd8eecd3ba8473
Supun Rashmika <supunrworks@gmail.com>
2026-07-02 22:08:26 +0530

    Added update arrays in state

--- src/Array/Array.jsx
+++ src/Array/Array.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Array() {
-  return (
-    <div>Array</div>
-  )
+
+    const [foods, setFoods] = useState(["Apple", "Banana", "Orange"])
+
+    function handleClick() {
+        const addItem = document.getElementById("inputItem").value
+        document.getElementById("inputItem").value = ""
+        setFoods([...foods, addItem])
+    }
+
+    return (
+        <div>
+            <h2>List of Fruits</h2>
+            <ul>
+                {foods.map((food, id) => <li key={id}>{food}</li>)}
+            </ul>
+            <input type="text" id='inputItem' placeholder='Enter your food' />
+            <button onClick={handleClick}>Add</button>
+        </div>
+    )
 }
 
 export default Array
