5ffe3ab0f5ad351e140fac4bff67f7944c5f60ca
Supun Rashmika <supunrworks@gmail.com>
2026-07-06 15:06:37 +0530

    Updated useRef lesson

--- src/UseRef/Ex02.jsx
+++ src/UseRef/Ex02.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function Ex02() {
+  const [name, setName] = useState("")
+
+  // const [render, setRender] = useState(0)         
+  const nameRef = useRef(0)                       // for useEffect
+  const inputRef = useRef ("")
+
+
+  useEffect(() => {
+    nameRef.current = nameRef.current + 1
+    inputRef.current.style.backgroundColor = "red"
+    inputRef.current.focus()  // Keep the input section selected
+  })
+
+  const inputHandle = (e) => {
+    setName(e.target.value)
+  }
+  const color = () =>{
+    inputRef.current.style.backgroundColor = "blue"   //Accesing DOM
+  }
+
   return (
-    <div>Ex02</div>
+    <div>
+      <p>Your name:{name}</p>
+      <input onChange={inputHandle} type="text" />
+      {/* <p>Renderd:{render}</p> */}
+      <p>Renderd:{nameRef.current}</p>
+
+      <input type="text" ref={inputRef}/>
+      <button onClick={color}>Change Color</button>
+
+    </div>
   )
 }
 
